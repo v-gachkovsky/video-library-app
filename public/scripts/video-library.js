80bd0856fc5a6e270174c35b55b8f72274606561
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-04 16:15:25 +0200

    Make videos in modal (#1)
    * make modal window for videos
    
    * fix modal window size; autoplay enabled
    
    * small refactoring

--- public/scripts/video-library.js
+++ public/scripts/video-library.js
@@ -5,13 +5,20 @@
   let library = [];
 
   function init() {
+    $(document).ready(() => {
+      $("#myModal").on("hidden.bs.modal", () => {
+        $("#iframeYoutube").attr("src","#");
+      });
+    });
+
     const ajax = new XMLHttpRequest();
 
     ajax.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         library = JSON.parse(this.responseText);
-        makeVideosThumbnails();
         console.log('library', library);
+
+        makeVideosThumbnails();
       }
     };
 
@@ -20,7 +27,32 @@
   }
 
   function makeVideosThumbnails() {
+    library.forEach(video => {
+      const thumbnail = document.createElement('img');
+      thumbnail.src = `http://img.youtube.com/vi/${video.videoId}/mqdefault.jpg`;
 
+      const thumbnailTitle = document.createElement('h4');
+      thumbnailTitle.className = 'thumbnail-video-title';
+      thumbnailTitle.innerHTML = video.title;
+
+      const thumbnailContainer = document.createElement('div');
+      thumbnailContainer.className = 'thumbnail-video-container';
+      thumbnailContainer.addEventListener('click', showVideo.bind(null, video.videoId));
+
+      thumbnailContainer.appendChild(thumbnail);
+      thumbnailContainer.appendChild(thumbnailTitle);
+
+      const videoLibraryContainer = document.getElementById('video-library');
+      videoLibraryContainer.appendChild(thumbnailContainer);
+    });
+  }
+
+  function showVideo(videoId) {
+    const iframe = document.getElementById('iframeYoutube');
+
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+
+    $("#myModal").modal("show");
   }
 
   init();
