fb7c46219e4d0befeeaf00b210f611150a51cd57
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-02-18 16:13:37 +0200

    make menu and videos loading (#5)

--- public/scripts/video-library.js
+++ public/scripts/video-library.js
@@ -1,33 +1,57 @@
 (function () {
+  const videoLibraryContainer = document.getElementById('video-library');
   const API = 'http://localhost:9000/api';
   const videoLibraryURL = `${API}/videos`;
+  const coursesURL = `${API}/courses`;
 
   let library = [];
+
+  function getCourses() {
+    return axios.get(coursesURL)
+  }
+
+  function getVideos(courseId) {
+    return axios.get(`${videoLibraryURL}/${courseId}`)
+  }
+
+  function openVideos(courseId) {
+
+
+    getVideos(courseId).then(response => {
+      library = response.data.videos
+
+      videoLibraryContainer.innerHTML = ''
+      makeVideosThumbnails()
+    })
+  }
 
   function init() {
     $(document).ready(() => {
       $("#myModal").on("hidden.bs.modal", () => {
         $("#iframeYoutube").attr("src","#");
       });
+
+      getCourses().then(({ data: courses }) => {
+        courses.forEach(course => {
+          const menuCourses = document.getElementById('menu-courses')
+          const menuItem = document.createElement('li')
+          const link = document.createElement('a')
+          link.href='#'
+          link.innerText = course.title
+
+          menuItem.appendChild(link)
+          menuCourses.appendChild(menuItem)
+          menuItem.addEventListener('click', openVideos.bind(null, course.id))
+        })
+      })
     });
 
-    const ajax = new XMLHttpRequest();
-
-    ajax.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        library = JSON.parse(this.responseText);
-        makeVideosThumbnails();
-      }
-    };
-
-    ajax.open('GET', videoLibraryURL, true);
-    ajax.send();
   }
 
   function makeVideosThumbnails() {
     library.forEach(video => {
       const thumbnail = document.createElement('img');
-      thumbnail.src = `http://img.youtube.com/vi/${video.videoId}/mqdefault.jpg`;
+      thumbnail.src = `http://img.youtube.com/vi/${video.videoCode}/mqdefault.jpg`;
 
       const thumbnailTitle = document.createElement('h4');
       thumbnailTitle.className = 'thumbnail-video-title';
@@ -35,12 +59,11 @@
 
       const thumbnailContainer = document.createElement('div');
       thumbnailContainer.className = 'thumbnail-video-container';
-      thumbnailContainer.addEventListener('click', showVideo.bind(null, video.videoId));
+      thumbnailContainer.addEventListener('click', showVideo.bind(null, video.videoCode));
 
       thumbnailContainer.appendChild(thumbnail);
       thumbnailContainer.appendChild(thumbnailTitle);
 
-      const videoLibraryContainer = document.getElementById('video-library');
       videoLibraryContainer.appendChild(thumbnailContainer);
     });
   }
