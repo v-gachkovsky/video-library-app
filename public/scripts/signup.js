6c0c5b0a6f6a938a9005bf6e92e9976f3e7d532c
Vladimir Gachkovsky <v.gachkovsky@gmail.com>
2018-03-29 01:20:29 +0300

    Signup 2 (#12)
    * sign in, sign out done
    
    * signup-2

---
+++ public/scripts/signup.js
@@ -0,0 +1,25 @@
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const firstName = document.getElementById('user-firstName').value;
+  const lastName = document.getElementById('user-lastName').value;
+  const email = document.getElementById('user-email').value;
+  const password = document.getElementById('user-password').value;
+
+  if (email.trim().length === 0) return;
+  if (password.trim().length < 6) return;
+
+  Axios.post('/signup', { 
+      firstName,
+      lastName,
+      email,
+      password }).then(({ data: { success } }) => {
+      console.log('success', success)
+    if (!success) return;
+
+    location.replace('/signin.html');
+  }).catch(error => {
+    console.log('error', error);
+  })
+});
